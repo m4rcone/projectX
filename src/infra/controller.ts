47ad895ab2aa7716c6b8d901909f1623e379e1c9
Marcone Boff <157663360+m4rcone@users.noreply.github.com>
2025-04-03 20:51:48 +0000

    feat: create `NotFoundError` and use in `controller.ts`

--- src/infra/controller.ts
+++ src/infra/controller.ts
@@ -2,16 +2,17 @@ import { NextResponse } from "next/server";
 import {
   InternalServerError,
   MethodNotAllowedError,
+  NotFoundError,
   ValidationError,
 } from "./errors";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function errorHandlerResponse(error: Error | any) {
-  if (error instanceof MethodNotAllowedError) {
-    return NextResponse.json(error, { status: error.statusCode });
-  }
-
-  if (error instanceof ValidationError) {
+  if (
+    error instanceof MethodNotAllowedError ||
+    ValidationError ||
+    NotFoundError
+  ) {
     return NextResponse.json(error, { status: error.statusCode });
   }
 
